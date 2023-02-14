1ea129236444c6fc7576e6db5bf9a7ff9b4a40bd
EWhalen2 <emmett.whalen@kellogg.northwestern.edu>
2023-02-14 02:02:21 +0000

    Did something in the posts lab

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,11 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def create
+    @posts = Post.new
+    @posts["author"] = params["posts"]["author"]
+    @posts["city"] = params["posts"]["body"]
+    @posts["state"] = params["posts"]["image"]
+  end
+
 end
